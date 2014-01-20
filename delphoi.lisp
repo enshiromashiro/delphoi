6582479b254b4fe6846a36d59a7bce3f3fc22b15
subaru45 <cerasus45@gmail.com>
2014-01-20 17:57:58 +0900

    Adds basics of user stream

--- delphoi.lisp
+++ delphoi.lisp
@@ -15,7 +15,6 @@
 (setf drakma:*drakma-default-external-format* :utf-8)
 (pushnew '("application" . "json") drakma:*text-content-types* :test #'equal)
 
-@export
 (defmacro make-access-token ()
   (let ((s (series:collect-first (series:scan-file "delphoi.conf"))))
     `(defparameter *access-token*
@@ -47,4 +46,27 @@
       *access-token*
       :request-method :post
       :user-parameters `(("status" . ,message))))))
+
+
+(defun print-tweet (json-string)
+  (ignore-errors
+    (json:with-decoder-simple-clos-semantics
+      (let* ((json:*json-symbols-package* :delphoi)
+             (x (json:decode-json-from-string json-string)))
+        (with-slots (text user) x
+          (with-slots (name screen--name) user
+            (format #.*standard-output* "~& ~%~a (~a)~&~a~%" screen--name name text)))))))
+
+@export
+(defun timeline ()
+  (bordeaux-threads:make-thread
+   (lambda ()
+     (with-open-stream (in (oauth:access-protected-resource
+                            "https://userstream.twitter.com/1.1/user.json"
+                            *access-token*
+                            :drakma-args '(:want-stream t)))
+       (loop for line = (read-line in nil)
+          while line
+          do (print-tweet line))))
+   :name "user-stream"))
 
